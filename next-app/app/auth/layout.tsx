0fe071f7bbf71ebae62b442947dbe4bcbb579392
Ayush Gupta <ag04062004@gmail.com>
2025-01-14 17:34:01 +0530

    added signin login

--- next-app/app/auth/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({children} : {children : React.ReactNode}){
-    return(
-        <div className="h-full w-full bg-[#ECF5FC] flex justify-center items-center">
-            {children}
-        </div>
-    )
-}
---
+++ nextjs-app/app/lib/email.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer"
+type SendEmailArg = {
+    email : string,
+    otp : string
+}
+export async function sendMail({email, otp }: SendEmailArg){
+    var transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: process.env.SMTP_USER_ID,
+          pass: process.env.SMTP_USER_PASSWORD
+        }
+    });
+    const result = await transport.sendMail({
+        from: 'Buy&Sell.com',
+        to: email,
+        subject: "Email Verification via OTP",
+        text: `OTP is ${otp}`
+    })
+    return result;
+}
