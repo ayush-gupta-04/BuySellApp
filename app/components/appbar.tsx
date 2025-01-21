440ecc9d272f25faa4208317cc5e99807d2a1638
ayush-gupta-04 <152642477+ayush-gupta-04@users.noreply.github.com>
2025-01-21 10:23:26 +0530

    classname -> className

--- app/components/appbar.tsx
+++ app/components/appbar.tsx
@@ -48,7 +48,7 @@ function Profile({name, email,image} : {name : string , email : string, image : 
             onClick={() => {setShow(!show)}}
             className="cursor-pointer"
             >
-                <Image src={image} height={40} width={40} alt="" classname = "rounded-full"/>
+                <Image src={image} height={40} width={40} alt="" className = "rounded-full"/>
             </div>}
         {!image && 
             <div
